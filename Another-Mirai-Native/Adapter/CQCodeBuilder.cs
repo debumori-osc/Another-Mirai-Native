3117e878c10ba8e61f9a56424af6881d2b626aae
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-08-16 17:10:13 +0800

    CQ码转消息链

--- Another-Mirai-Native/Adapter/CQCodeBuilder.cs
+++ Another-Mirai-Native/Adapter/CQCodeBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using Another_Mirai_Native.Adapter.CQCode;
 using Another_Mirai_Native.Adapter.CQCode.Expand;
+using Another_Mirai_Native.Adapter.CQCode.Model;
 using Another_Mirai_Native.DB;
 using Another_Mirai_Native.Enums;
 using Another_Mirai_Native.Native;
@@ -98,5 +99,81 @@ namespace Another_Mirai_Native.Adapter
             }
             return Result.ToString();
         }
+        public static List<MiraiMessageBase> BuildMessageChains(string message)
+        {
+            List<MiraiMessageBase> result = new List<MiraiMessageBase>();
+            var list = CQCodeModel.Parse(message);
+            foreach (var item in list)
+            {
+                message = message.Replace(item.ToString(), "<!cqcode!>[$$]");
+            }
+            List<string> p = new();
+            string tmp = "";
+            for (int i = 0; i < message.Length; i++)
+            {
+                tmp += message[i];
+                if (tmp.EndsWith("[$$]"))
+                {
+                    p.Add(tmp[..^4]);
+                    tmp = "";
+                }
+            }
+            int cqcode_index = 0;
+            for (int i = 0; i < p.Count; i++)
+            {
+                MiraiMessageBase messageBase;
+                if (p[i] == "<!cqcode!>")
+                {
+                    messageBase = ParseCQCode2MiraiMessageBase(list[cqcode_index]);
+                    cqcode_index++;
+                }
+                else
+                {
+                    messageBase = new MiraiMessageTypeDetail.Plain { text = p[i] };
+                }
+                result.Add(messageBase);
+            }
+            return result;
+        }
+
+        private static MiraiMessageBase ParseCQCode2MiraiMessageBase(CQCodeModel cqcode)
+        {
+            switch (cqcode.Function)
+            {
+                case CQCode.Enum.CQFunction.Face:
+                    return new MiraiMessageTypeDetail.Face { faceId = Convert.ToInt32(cqcode.Items["id"]) };
+                case CQCode.Enum.CQFunction.Bface:
+                    return new MiraiMessageTypeDetail.MarketFace { id = Convert.ToInt32(cqcode.Items["id"]) };
+                case CQCode.Enum.CQFunction.Image:
+                    string picPath = cqcode.Items["file"];
+                    picPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, picPath);
+                    if (cqcode.Items.ContainsKey("flash"))
+                    {
+                        return new MiraiMessageTypeDetail.FlashImage { path = picPath };
+                    }
+                    return new MiraiMessageTypeDetail.Image { path = picPath };
+                case CQCode.Enum.CQFunction.Record:
+                    string recordPath = cqcode.Items["file"];
+                    recordPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, recordPath);
+                    return new MiraiMessageTypeDetail.Voice { path = recordPath };
+                case CQCode.Enum.CQFunction.At:
+                    return new MiraiMessageTypeDetail.At { target = Convert.ToInt64(cqcode.Items["qq"]) };
+                case CQCode.Enum.CQFunction.Dice:
+                    return new MiraiMessageTypeDetail.Dice { value = Convert.ToInt32(cqcode.Items["point"]) };
+                case CQCode.Enum.CQFunction.Music:
+                    return new MiraiMessageTypeDetail.MusicShare { brief = cqcode.Items["content"], jumpUrl = cqcode.Items["url"], musicUrl = cqcode.Items["audio"], pictureUrl = cqcode.Items["imageUrl"], title = cqcode.Items["title"] };
+                case CQCode.Enum.CQFunction.Rich:
+                    return (object)cqcode.Items["type"] switch
+                    {
+                        "xml" => new MiraiMessageTypeDetail.Xml { xml = cqcode.Items["content"] },
+                        "json" => new MiraiMessageTypeDetail.Json { json = cqcode.Items["content"] },
+                        "app" => new MiraiMessageTypeDetail.App { content = cqcode.Items["content"] },
+                        _ => null,
+                    };
+                default:
+                    return null;
+            }
+        }
+
     }
 }
