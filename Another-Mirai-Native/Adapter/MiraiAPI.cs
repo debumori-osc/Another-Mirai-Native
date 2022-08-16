6ebcff6a4216f12b1479cf09ea3b7459920a8436
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-08-16 17:09:59 +0800

    调用MiraiAPI类

--- Another-Mirai-Native/Adapter/MiraiAPI.cs
+++ Another-Mirai-Native/Adapter/MiraiAPI.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Another_Mirai_Native.Adapter.CQCode.Model;
+using Another_Mirai_Native.Enums;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Another_Mirai_Native.Adapter
 {
-    internal class MiraiAPI
+    public static class MiraiAPI
     {
+        public static int SendGroupMessage(long group, string message)
+        {
+            MiraiMessageBase[] msgChains = CQCodeBuilder.BuildMessageChains(message).ToArray();
+            object request = new
+            {
+                sessionKey = MiraiAdapter.Instance.SessionKey_Message,
+                target = group,
+                messageChain = msgChains.ToJson()
+            };
+            MiraiAdapter.Instance.CallMiraiAPI(request);
+            return 0;
+        }
+        public static int SendFriendMessage(long QQ, string message)
+        {
+            return 0;
+        }
     }
 }
