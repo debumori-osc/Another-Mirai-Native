cbad18ba2a43fa0e3c9ead83c4db20a6c0e99792
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-08-16 10:54:53 +0800

    消息数组转CQ码

--- Another-Mirai-Native/Adapter/CQCodeBuilder.cs
+++ Another-Mirai-Native/Adapter/CQCodeBuilder.cs
@@ -1,17 +1,104 @@
-﻿using Another_Mirai_Native.Enums;
+﻿using Another_Mirai_Native.Adapter.CQCode;
+using Another_Mirai_Native.Enums;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Another_Mirai_Native.Adapter
 {
-    public class CQCodeBuilder
+    public static class CQCodeBuilder
     {
-        public static string Parse(List<MiraiMessageBase> chainMsg)
+        public static string Parse (List<MiraiMessageBase> chainMsg)
         {
-            throw new NotImplementedException();
+            StringBuilder Result = new();
+            foreach (var item in chainMsg)
+            {
+                switch (item.messageType)
+                {
+                    case MiraiMessageType.Source:
+                        break;
+                    case MiraiMessageType.Quote:
+                        break;
+                    case MiraiMessageType.At:
+                        var at = (MiraiMessageTypeDetail.At)item;
+                        Result.Append(CQApi.CQCode_At(at.target));
+                        break;
+                    case MiraiMessageType.AtAll:
+                        Result.Append(CQApi.CQCode_AtAll());
+                        break;
+                    case MiraiMessageType.Face:
+                        var face = (MiraiMessageTypeDetail.Face)item;
+                        Result.Append(CQApi.CQCode_Face(face.faceId));
+                        break;
+                    case MiraiMessageType.Plain:
+                        var plain = (MiraiMessageTypeDetail.Plain)item;
+                        Result.Append(plain.text);
+                        break;
+                    case MiraiMessageType.Image:
+                        var image = (MiraiMessageTypeDetail.Image)item;
+                        string imgId = image.imageId.Replace("-", "");
+                        Directory.CreateDirectory("data/image");
+                        File.WriteAllText($"data/image/{imgId}.cqimg", $"[image]\nmd5=0\nsize=0\nurl={image.url}");
+                        Result.Append($"[CQ:image,file={imgId}]");
+                        break;
+                    case MiraiMessageType.FlashImage:
+                        var flashImage = (MiraiMessageTypeDetail.FlashImage)item;
+                        string flashImgId = flashImage.imageId.Replace("-", "");
+                        Directory.CreateDirectory("data/image");
+                        File.WriteAllText($"data/image/{flashImgId}.cqimg", $"[image]\nmd5=0\nsize=0\nurl={flashImage.url}");
+                        Result.Append($"[CQ:image,file={flashImgId},flash=true]");
+                        break;
+                    case MiraiMessageType.Voice:
+                        var voice = (MiraiMessageTypeDetail.Voice)item;
+                        string voiceId = voice.voiceId.Replace(".amr", "");
+                        Directory.CreateDirectory("data/record");
+                        File.WriteAllText($"data/image/{voiceId}.cqrecord", $"[record]\nurl={voice.url}");
+                        Result.Append($"[CQ:record,file={voice.voiceId}]");
+                        break;
+                    case MiraiMessageType.Xml:
+                        var xml = (MiraiMessageTypeDetail.Xml)item;
+                        Result.Append($"[CQ:rich,type=xml,content={xml.xml}]");
+                        break;
+                    case MiraiMessageType.Json:
+                        var json = (MiraiMessageTypeDetail.Json)item;
+                        Result.Append($"[CQ:rich,type=json,content={json.json}]");
+                        break;
+                    case MiraiMessageType.App:
+                        var app = (MiraiMessageTypeDetail.App)item;
+                        Result.Append($"[CQ:rich,type=app,content={app.content}]");
+                        break;
+                    case MiraiMessageType.Poke:
+                        var poke = (MiraiMessageTypeDetail.Poke)item;
+                        Result.Append($"[CQ:poke,name={poke.name},]");
+                        break;
+                    case MiraiMessageType.Dice:
+                        var dice = (MiraiMessageTypeDetail.Dice)item;
+                        Result.Append($"[CQ:dice,point={dice.value}]");
+                        break;
+                    case MiraiMessageType.MarketFace:
+                        var marketFace = (MiraiMessageTypeDetail.MarketFace)item;
+                        Result.Append($"[CQ:bigface,id={marketFace.id}]");
+                        break;
+                    case MiraiMessageType.MusicShare:
+                        var musicShare = (MiraiMessageTypeDetail.MusicShare)item;
+                        Result.Append(CQApi.CQCode_DIYMusic(musicShare.jumpUrl, musicShare.musicUrl, musicShare.title, musicShare.brief, musicShare.pictureUrl));
+                        break;
+                    case MiraiMessageType.Forward:
+                        break;
+                    case MiraiMessageType.File:
+                        var file = (MiraiMessageTypeDetail.File)item;
+                        Result.Append($"[CQ:file,]");
+                        break;
+                    case MiraiMessageType.MiraiCode:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return Result.ToString();
         }
     }
 }
--- Another-Mirai-Native/Enums/MiraiMessageType.cs
+++ Another-Mirai-Native/Enums/MiraiMessageType.cs
@@ -9,6 +9,7 @@ namespace Another_Mirai_Native.Enums
     public interface MiraiMessageBase
     {
         string type { get; set; }
+        MiraiMessageType messageType { get; set; }
     }
     public enum MiraiMessageType
     {
@@ -37,6 +38,7 @@ namespace Another_Mirai_Native.Enums
         public class Source : MiraiMessageBase
         {
             public string type { get; set; }
+            public MiraiMessageType messageType { get; set; } = MiraiMessageType.Source;
             public int id { get; set; }
             public int time { get; set; }
         }
@@ -49,6 +51,8 @@ namespace Another_Mirai_Native.Enums
             public int senderId { get; set; }
             public long targetId { get; set; }
             public Origin[] origin { get; set; }
+            public MiraiMessageType messageType { get; set; } = MiraiMessageType.Quote;
+
             public class Origin
             {
                 public string type { get; set; }
@@ -59,6 +63,7 @@ namespace Another_Mirai_Native.Enums
         public class At : MiraiMessageBase
         {
             public string type { get; set; }
+            public MiraiMessageType messageType { get; set; } = MiraiMessageType.At;
             public int target { get; set; }
             public string display { get; set; }
         }
@@ -66,11 +71,13 @@ namespace Another_Mirai_Native.Enums
         public class AtAll : MiraiMessageBase
         {
             public string type { get; set; }
+            public MiraiMessageType messageType { get; set; } = MiraiMessageType.AtAll;
         }
 
         public class Face : MiraiMessageBase
         {
             public string type { get; set; }
+            public MiraiMessageType messageType { get; set; } = MiraiMessageType.Face;
             public int faceId { get; set; }
             public string name { get; set; }
         }
@@ -78,70 +85,80 @@ namespace Another_Mirai_Native.Enums
         public class Plain : MiraiMessageBase
         {
             public string type { get; set; }
+            public MiraiMessageType messageType { get; set; } = MiraiMessageType.Plain;
             public string text { get; set; }
         }
 
         public class Image : MiraiMessageBase
         {
             public string type { get; set; }
+            public MiraiMessageType messageType { get; set; } = MiraiMessageType.Image;
             public string imageId { get; set; }
             public string url { get; set; }
-            public object path { get; set; }
-            public object base64 { get; set; }
+            public string path { get; set; }
+            public string base64 { get; set; }
         }
 
         public class FlashImage : MiraiMessageBase
         {
             public string type { get; set; }
+            public MiraiMessageType messageType { get; set; } = MiraiMessageType.FlashImage;
             public string imageId { get; set; }
             public string url { get; set; }
-            public object path { get; set; }
-            public object base64 { get; set; }
+            public string path { get; set; }
+            public string base64 { get; set; }
         }
 
         public class Voice : MiraiMessageBase
         {
             public string type { get; set; }
+            public MiraiMessageType messageType { get; set; } = MiraiMessageType.Voice;
             public string voiceId { get; set; }
             public string url { get; set; }
-            public object path { get; set; }
-            public object base64 { get; set; }
+            public string path { get; set; }
+            public string base64 { get; set; }
             public int length { get; set; }
         }
 
         public class Xml : MiraiMessageBase
         {
             public string type { get; set; }
+            public MiraiMessageType messageType { get; set; } = MiraiMessageType.Xml;
             public string xml { get; set; }
         }
 
         public class Json : MiraiMessageBase
         {
             public string type { get; set; }
+            public MiraiMessageType messageType { get; set; } = MiraiMessageType.Json;
             public string json { get; set; }
         }
 
         public class App : MiraiMessageBase
         {
             public string type { get; set; }
+            public MiraiMessageType messageType { get; set; } = MiraiMessageType.App;
             public string content { get; set; }
         }
 
         public class Poke : MiraiMessageBase
         {
             public string type { get; set; }
+            public MiraiMessageType messageType { get; set; } = MiraiMessageType.Poke;
             public string name { get; set; }
         }
 
         public class Dice : MiraiMessageBase
         {
             public string type { get; set; }
+            public MiraiMessageType messageType { get; set; } = MiraiMessageType.Dice;
             public int value { get; set; }
         }
 
         public class MarketFace : MiraiMessageBase
         {
             public string type { get; set; }
+            public MiraiMessageType messageType { get; set; } = MiraiMessageType.MarketFace;
             public int id { get; set; }
             public string name { get; set; }
         }
@@ -149,6 +166,7 @@ namespace Another_Mirai_Native.Enums
         public class MusicShare : MiraiMessageBase
         {
             public string type { get; set; }
+            public MiraiMessageType messageType { get; set; } = MiraiMessageType.MusicShare;
             public string kind { get; set; }
             public string title { get; set; }
             public string summary { get; set; }
@@ -161,6 +179,7 @@ namespace Another_Mirai_Native.Enums
         public class Forward : MiraiMessageBase
         {
             public string type { get; set; }
+            public MiraiMessageType messageType { get; set; } = MiraiMessageType.Forward;
             public Nodelist[] nodeList { get; set; }
             public class Nodelist
             {
@@ -175,6 +194,7 @@ namespace Another_Mirai_Native.Enums
         public class File : MiraiMessageBase
         {
             public string type { get; set; }
+            public MiraiMessageType messageType { get; set; } = MiraiMessageType.File;
             public string id { get; set; }
             public string name { get; set; }
             public int size { get; set; }
@@ -183,6 +203,7 @@ namespace Another_Mirai_Native.Enums
         public class MiraiCode : MiraiMessageBase
         {
             public string type { get; set; }
+            public MiraiMessageType messageType { get; set; } = MiraiMessageType.MiraiCode;
             public string code { get; set; }
         }
     }
