d3c12e57a9a4011b439d54dfd64537166d90c685
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-08-16 16:04:19 +0800

    ws鉴权

--- Another-Mirai-Native/WsServer.cs
+++ Another-Mirai-Native/WsServer.cs
@@ -15,14 +15,45 @@ namespace Another_Mirai_Native
     {
         private class Handler : WebSocketBehavior
         {
+            private WsClientType ClientType = WsClientType.UnAuth;
             protected override void OnMessage(MessageEventArgs e)
             {
                 JObject json = JObject.Parse(e.Data);
+                var data = json["data"];
+                switch (json["type"].ToObject<WsServerFunction>())
+                {
+                    case WsServerFunction.Info:
+                        switch (data["role"].ToObject<WsClientType>())
+                        {
+                            case WsClientType.CQP:
+                            case WsClientType.WebUI:
+                                string key = data["key"].ToString();
+                                if (key == ConfigHelper.GetConfig<string>("WsServer_Key"))
+                                {
+                                    ClientType = data["role"].ToObject<WsClientType>();
+                                }
+                                break;
+                            case WsClientType.UnAuth:
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                if(ClientType == WsClientType.UnAuth)
+                {
+                    Send(WsServerFunction.UnAuth, "");
+                    return;
+                }
                 switch (json["type"].ToObject<WsServerFunction>())
                 {
                     case WsServerFunction.AddLog:
                         break;
                     case WsServerFunction.GetLog:
+                        break;
+                    case WsServerFunction.CallMiraiAPI:
                         break;
                     case WsServerFunction.CallCQFunction:
                         break;
@@ -49,6 +80,10 @@ namespace Another_Mirai_Native
                     default:
                         break;
                 }
+            }
+            public void Send(WsServerFunction type, object data)
+            {
+                 Send(new { type, data }.ToJson());
             }
         }
         public static WsServer Instance { get; set; }
