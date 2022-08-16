a27bf086a43f29a2c55c81e68cf124866b07258a
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-08-16 10:54:16 +0800

    添加CQ码解析类

---
+++ Another-Mirai-Native/CQCode/Core/Kernel32.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Another_Mirai_Native.Adapter.CQCode.Core
+{
+	internal class Kernel32
+	{
+		[DllImport ("kernel32.dll", EntryPoint = "lstrlenA", CharSet = CharSet.Ansi)]
+		public extern static int LstrlenA (IntPtr ptr);
+	}
+}
