4991111c9e3c70daa9bbd4f578a9919cf1216e8b
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-08-17 11:08:22 +0800

    好友/加群申请缓存

---
+++ Another-Mirai-Native/Adapter/Cache.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Another_Mirai_Native.Adapter
+{
+    public static class Cache
+    {
+        public static Dictionary<long, (long, string)> FriendRequset { get; set; } = new();
+        public static Dictionary<long, (long, string, long, string)> GroupRequset { get; set; } = new();
+    }
+}
